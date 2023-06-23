8939ed9708c872791a178c149d58b729d98af60a
GBoteon <boteongustavo@gmail.com>
2023-06-22 23:48:10 -0300

    Minor variable refactor, for clarity

--- Program.cs
+++ Program.cs
@@ -10,13 +10,13 @@ namespace StockQuoteAlert
         {
 
             string symbol = "PETR4";
-            decimal entryPrice = 30.43m;
-            decimal sellPrice = 30.10m;
+            decimal refEntryPrice = 30.43m;
+            decimal refSellPrice = 30.10m;
             if (args.Length == 3)
             {
                 symbol = args[0];
-                entryPrice = decimal.Parse(args[1]);
-                sellPrice = decimal.Parse(args[2]);
+                refEntryPrice = decimal.Parse(args[1]);
+                refSellPrice = decimal.Parse(args[2]);
             }
 
             string emailDestino = ConfigReader.ReadSetting("EmailDestino");
@@ -43,13 +43,13 @@ namespace StockQuoteAlert
 
                 Console.WriteLine($"Cotação atual de {symbol}: {quote}");
 
-                if (quote > entryPrice)
+                if (quote > refSellPrice)
                 {
-                    EmailService.SendEmail(emailDestino, $"Venda recomendada do symbol {symbol}", $"O preço atual ({quote}) está acima do preço de venda ({entryPrice})", smtpHost, smtpPort, smtpUsername, smtpPassword);
+                    EmailService.SendEmail(emailDestino, $"Venda recomendada do symbol {symbol}", $"O preço atual ({quote}) está acima do preço de venda ({refSellPrice})", smtpHost, smtpPort, smtpUsername, smtpPassword);
                 }
-                else if (quote < sellPrice)
+                else if (quote < refEntryPrice)
                 {
-                    EmailService.SendEmail(emailDestino, $"Compra recomendada do symbol {symbol}", $"O preço atual ({quote}) está abaixo do preço de compra ({sellPrice})", smtpHost, smtpPort, smtpUsername, smtpPassword);
+                    EmailService.SendEmail(emailDestino, $"Compra recomendada do symbol {symbol}", $"O preço atual ({quote}) está abaixo do preço de compra ({refEntryPrice})", smtpHost, smtpPort, smtpUsername, smtpPassword);
                 }
 
                 Thread.Sleep(10000); // Aguardar 10 segundos antes de verificar novamente
